7771210a758160edc6f803a6301c3c7a8025e69e
Sachin Basyal <sachinbasyal@gmail.com>
2024-06-09 18:29:23 -0400

    Connect to MongoDB Atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
- 
+//require('dotenv').config({path: './env'}) // common JS  style
+import dotenv from 'dotenv' // module JS
+import connectDB from './db/index.js'
+
+
+dotenv.config({path:'./env'})
+
+connectDB(); //connnect to MongoDB 
+
+
+/* 1st Method to connect DB
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express";
+const app = express();
+
+Best practice: Always use Async / Await() and wrap with Try-Catch() /Promises while connecting to DB. [DB is always supposed to be on another continent..]
+
+IIFE ()();
+;(async()=>{
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("Error", (error)=>{
+      console.log("Error:", error)
+      throw error
+    })
+
+    app.listen(process.env.PORT, ()=>{
+      console.log(`App is listening on port ${process.env.PORT} `);
+    })
+
+  } catch (error) {
+    console.log("Error: ",error)
+    throw error
+  }
+})();
+*/
